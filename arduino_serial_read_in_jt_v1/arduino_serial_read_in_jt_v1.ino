fef735d198bb53aaf64426829c058ebfdb612ca9
JT-CH <64595993+JT-CH@users.noreply.github.com>
2020-05-05 09:53:15 +0100

    Adding first folder of Arduino code

---
+++ arduino_serial_read_in_jt_v1/arduino_serial_read_in_jt_v1.ino
@@ -0,0 +1,54 @@
+//Sample using LiquidCrystal library
+#include <LiquidCrystal.h>
+
+/*******************************************************
+
+This program will display odometer reading sent from PC --> Arduino along serial port COM3
+v1.0
+
+********************************************************/
+
+// select the pins used on the LCD panel
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+long odom = 123456;
+
+void setup()
+{
+ Serial.begin(9600); // open serial port
+  
+ lcd.begin(16, 2);              // start the library
+ lcd.setCursor(0,0);
+ lcd.print("SlowLaneTrucking"); // print a simple message
+ lcd.setCursor(0,1);            // move to the begining of the second(1) line
+ lcd.print("Mileage:");
+}
+ 
+void loop()
+{
+ lcd.setCursor(10,0);            // move cursor to second line "1" and 9 spaces over
+// lcd.print(millis()/1000);      // display seconds elapsed since power-up
+ lcd.setCursor(9,1);             // move cursor 9 spaces over on second(1) line
+ 
+ // get odometer reading from serial
+
+/*******************************************************
+
+Many thanks to (EconJack) for Arduino serial receive code: https://forum.arduino.cc/index.php?topic=498441.0
+
+********************************************************/
+
+int charsReceived;
+  char inputBuffer[7];            // Size of buffer to read from serial, make whatever size you need plus one
+
+  if (Serial.available()) 
+  {
+    charsReceived = Serial.readBytesUntil('\n', inputBuffer, sizeof(inputBuffer) - 1);  // Save room for NULL
+    inputBuffer[charsReceived] = NULL;      // Make it a string
+    Serial.print("there were ");
+    Serial.print(charsReceived);
+    Serial.print(" chars received, which are: ");
+    Serial.println(inputBuffer);   
+  }
+ 
+ lcd.print(inputBuffer);
+}
